47319999aeb4c4e79ae8f5114e9246732aac05a5
salalba <saljan93@live.com>
2021-06-19 15:23:00 +0200

    docker compose

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Chatbot Stop Hate
 
 docker network create -d overlay stop_hate
+docker network create stop-hate
 
 https://github.com/RasaHQ/rasa-for-beginners
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # ...
-SERVICE_CONTAINER_1="bot_stop_hate_core_ai"
-SERVICE_NAME="stop-hate-ai"
+SERVICE_CONTAINER_1="chatbot_bot_core_ai_service_1"
+SERVICE_CONTAINER_2="chatbot_bot_backend_service_1"
+SERVICE_IMAGE_1="chatbot_bot_core_ai_service"
+SERVICE_IMAGE_2="chatbot_bot_backend_service"
 
 
 # ...
@@ -10,5 +12,9 @@ clear
 echo "========================== START ============================="
 echo ">> CLEANS <<"
 docker stop $SERVICE_CONTAINER_1 || true && docker rm $SERVICE_CONTAINER_1 || true
-docker-compose --file docker-compose.yml up 
+docker stop $SERVICE_CONTAINER_2 || true && docker rm $SERVICE_CONTAINER_2 || true
+docker rmi $SERVICE_IMAGE_1 || true
+docker rmi $SERVICE_IMAGE_2 || true
+docker-compose down
+docker-compose --file docker-compose.yml up -d
 echo "========================== END ==============================="
